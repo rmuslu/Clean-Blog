3de1f3ddeafbcbff9291e51e8f9fdc167f516d37
rmuslu <49349809+rmuslu@users.noreply.github.com>
2021-05-26 16:52:47 +0300

    Static files and EJS

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express');
-
+const path = require('path');
+const ejs = require('ejs');
 const app = express();
 
-app.get('/', (req, res) => {
 
-  const photo = {
-    id: 1,
-    titşe: "Blog Title",
-    description: "Blog description"
-  }
-  res.send(photo)
-})
+//TEMPLATE ENGINE
+app.set("view engine","ejs");
+
+
+//MIDDLEWARE
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  //res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+ res.render('index');
+});
 
 const port = 3000;
 app.listen(port, () => {
